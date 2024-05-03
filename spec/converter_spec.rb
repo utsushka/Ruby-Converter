8a53a623c0c74c1fad6ca51cc398eef8b715c6cb
Semechka <115107268+utsushka@users.noreply.github.com>
2024-05-03 18:00:21 +0000

    Написал заглушки для будущих тестов

---
+++ spec/converter_spec.rb
@@ -0,0 +1,17 @@
+require 'converter'
+
+RSpec.describe Converter do
+  let(:converter) { Converter.new }
+
+  describe '#convert_length' do
+  end
+
+  describe '#convert_weight' do
+  end
+
+  describe '#convert_volume' do
+  end
+
+  describe '#convert_kitchen_units' do
+  end
+end
