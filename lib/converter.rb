36bc3c7f3f50870a4c3f4f226980ec0ed62770ac
Flluent <Sergeyyashonkov@bk.ru>
2024-05-03 20:37:04 +0300

    Сделал заглушку для будущего класса Converter

---
+++ lib/converter.rb
@@ -0,0 +1,21 @@
+class Converter
+  LENGTH_CONVERSIONS = { }
+
+  WEIGHT_CONVERSIONS = { }
+
+  VOLUME_CONVERSIONS = { }
+
+  KITCHEN_UNITS_CONVERSIONS = { }
+
+  def convert_length(value, from_unit, to_unit)
+  end
+
+  def convert_weight(value, from_unit, to_unit)
+  end
+
+  def convert_volume(value, from_unit, to_unit)
+  end
+
+  def convert_kitchen_units(value, from_unit, to_unit)
+  end
+end
