1e7249acaf296f2333b4d977567dbd6cc7d14253
Semechka <115107268+utsushka@users.noreply.github.com>
2024-05-05 09:07:06 +0000

    Разбил ещё на два модуля

---
+++ lib/force_converter.rb
@@ -0,0 +1,15 @@
+module ForceConverter
+  NEWTON = :newton
+  KILOGRAM_FORCE = :kilogram_force
+  POUND_FORCE = :pound_force
+
+  CONVERSIONS = {
+    NEWTON => { NEWTON => 1, KILOGRAM_FORCE => 0.101972, POUND_FORCE => 0.224809 },
+    KILOGRAM_FORCE => { NEWTON => 9.80665, KILOGRAM_FORCE => 1, POUND_FORCE => 2.20462 },
+    POUND_FORCE => { NEWTON => 4.44822, KILOGRAM_FORCE => 0.453592, POUND_FORCE => 1 }
+  }.freeze
+
+  def convert_force(value, from_unit, to_unit)
+    (value.to_f * CONVERSIONS[from_unit][to_unit]).round(6)
+  end
+end
